31c2e2ffa060a392eda1768b5b6dd88c50a08166
Roland <roland.onofrej@akademiasovy.sk>
2016-12-01 10:28:06 +0100

    New version of project

--- Employee.h
+++ Employee.h
@@ -1,7 +1,8 @@
  #include <stdio.h>
  #define SIZE 30
  
- struct person{
+// global variables
+struct person{
  int day;
  int month;
  int year;
@@ -11,8 +12,7 @@
  char lastname[30];
 };
 
-// global variables
-struct person employees[SIZE];
+
 
 
 bool isValidDate(int dd, int mm, int yy);
@@ -20,7 +20,7 @@ bool isLeapYear(int year);
 bool isValidSex(char z);
 FILE* openFile(char* fileName, char* mode);
 void closeFile(FILE *fr);
-bool getDataFromFile(FILE *fr);
+int getDataFromFile(FILE *fr, struct person employees[]);
 void sortDataByBirthday(struct person *p, int count);
 void sortDataByLastName(struct person *p, int count);
 void printData(struct person *p, int count);
