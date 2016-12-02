1c3d2ff185418a14d6a590837c11dd7a9fcef840
Rastislav Ruščák <rastislav.ruscak@akademiasovy.sk>
2016-12-02 11:24:51 +0100

    updated copyright restriction wording

--- Rastislav Ruscak/main.cpp
+++ Rastislav Ruscak/main.cpp
@@ -1,8 +1,12 @@
 /**
-* @author: Rastislav RuöË·k
+* @author: Rastislav Ru≈°√®√°k
 * @date: 1.12.2016
 * @description: Program print sorted correct data of employees, their salary statistics and generate file with e-mails of employees.
 * @copyright: All rights reserved.
+* 	No part of this publication may be reproduced, distributed, or transmitted in any form or by any means, 
+*	including photocopying, recording, or other electronic or mechanical methods, without the prior written 
+*	permission of the publisher, except in the case of brief quotations embodied in critical reviews and 
+*	certain other noncommercial uses permitted by copyright law.
 */
 #include <stdio.h>
 #include <string.h>
